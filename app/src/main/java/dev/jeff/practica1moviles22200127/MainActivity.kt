8219b1db26625afb1302212d9a85e9cfc0479a61
Jefferson <Jefferson@JEFFFERSON>
2025-10-18 08:12:15 -0500

    feat: Initial project setup
    Initialize the Android project with a basic Jetpack Compose setup.
    
    This commit includes the following:
    - Add Gradle wrapper and configuration files.
    - Set up basic app structure with `MainActivity`, `AndroidManifest.xml`, and default resources.
    - Configure dependencies for Jetpack Compose, Core KTX, and testing libraries.
    - Add initial theme, color, and typography definitions for the app.
    - Include standard `.gitignore` files for Android Studio and the project.
    - Define project settings for the IDE, including compiler and run configurations.

---
+++ app/src/main/java/dev/jeff/practica1moviles22200127/MainActivity.kt
@@ -0,0 +1,48 @@
+package dev.jeff.practica1moviles22200127
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.jeff.practica1moviles22200127.ui.theme.Practica1Moviles22200127Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Practica1Moviles22200127Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                        // Print(holamundo)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Practica1Moviles22200127Theme {
+        Greeting("Android")
+    }
+}
